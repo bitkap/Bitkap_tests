822484fedc8a0a410c32491768dd91d4f543ae08
baudouin-22 <bakengfack@gmail.com>
2021-12-16 17:40:44 +0100

    initialisation

---
+++ events/events-manager/src/main/java/bitkap/eventsmanager/EventsManagerApplication.java
@@ -0,0 +1,13 @@
+package bitkap.eventsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsManagerApplication.class, args);
+    }
+
+}
---
+++ events/events-manager/src/test/java/bitkap/eventsmanager/EventsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package bitkap.eventsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
